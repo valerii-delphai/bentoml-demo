64b2b9f1cf39a838793fd77bd530a1faa8835c32
valerii-delphai <102948264+valerii-delphai@users.noreply.github.com>
2022-04-10 22:46:18 +0300

    Update save.py

--- src/save.py
+++ src/save.py
@@ -4,13 +4,16 @@ from pathlib import Path
 
 source = Path(__file__).parent
 
+# call predefined earlier bentoml object
 brp = BikeRentalsPredictionService()
 
+# for two models...
 for i in ["casual", "registered"]:
+    # unpickle
     with open(f'{source}/model_{i}.pkl', 'rb') as f:
         model = pickle.load(f)
-
+    # pack
     brp.pack(f"model_{i}",  model)
 
-
+# convert to service
 saved_path = brp.save()
